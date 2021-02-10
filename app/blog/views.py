d520603c0f0b8825be2f4e588ea377ffd3f3336a
Deniz Akpınar <ddenizakpinar@gmail.com>
2021-02-10 20:09:46 +0300

    Add user and post models

---
+++ app/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
